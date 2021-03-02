6a7876809aad99627b010cef02ed42eee1bfa18a
Tauã Negri <tauanegri@corpstek.com.br>
2021-03-02 09:59:49 -0300

    hotfix for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/frontend/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
 
   /*  devServer: {
     host: "192.168.0.12"
